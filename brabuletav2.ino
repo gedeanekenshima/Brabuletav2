6f74d65e30981ecec8cfbfb6722c9b15c69b465f
Gedeane Kenshima <gedeanegomes@hotmail.com>
2020-07-02 21:54:01 -0300

    Update brabuletav2.ino

--- brabuletav2.ino
+++ brabuletav2.ino
@@ -1,96 +1,101 @@
-// A basic everyday NeoPixel strip test program.
-
-// NEOPIXEL BEST PRACTICES for most reliable operation:
-// - Add 1000 uF CAPACITOR between NeoPixel strip's + and - connections.
-// - MINIMIZE WIRING LENGTH between microcontroller board and first pixel.
-// - NeoPixel strip's DATA-IN should pass through a 300-500 OHM RESISTOR.
-// - AVOID connecting NeoPixels on a LIVE CIRCUIT. If you must, ALWAYS
-//   connect GROUND (-) first, then +, then data.
-// - When using a 3.3V microcontroller with a 5V-powered NeoPixel strip,
-//   a LOGIC-LEVEL CONVERTER on the data line is STRONGLY RECOMMENDED.
-// (Skipping these may work OK on your workbench but can fail in the field)
-
+/* Sketch baseado no código de Bill Earl:
+ *  https://learn.adafruit.com/multi-tasking-the-arduino-part-1/a-clean-sweep
+ *  E sketch Strandtest, da biblioteca Adafruit_NeoPixel
+ *  Modificado e adaptado por Gedeane Kenshima
+ *  27/06/2020
+ */
+#include <Servo.h> 
 #include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
-#endif
 
-// Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1:
-#define LED_PIN    2
+#define NUM_PIXELS 16
 
-// How many NeoPixels are attached to the Arduino?
-#define LED_COUNT 16
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, 2, NEO_GRB + NEO_KHZ800);
+unsigned long interval=50;  // intervalo de 50 ms
 
-// Declare our NeoPixel strip object:
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
-// Argument 1 = Number of pixels in NeoPixel strip
-// Argument 2 = Arduino pin number (most are valid)
-// Argument 3 = Pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-
-#include <Servo.h>
-
-Servo myservo1,myservo2;  // create servo object to control a servo
-
-  int pos=0;              // current servo position 
-  int increment=1;        // increment to move for each interval
-  int  updateInterval=15;      // interval between updates
-  unsigned long lastUpdate=0; // last update of position
-
-// setup() function -- runs once at startup --------------------------------
-
-// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
-void rainbow(int wait) {
-  // Hue of first pixel runs 5 complete loops through the color wheel.
-  // Color wheel has a range of 65536 but it's OK if we roll over, so
-  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
-  // means we'll make 5*65536/256 = 1280 passes through this outer loop:
-  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
-    for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
-      // Offset pixel hue by an amount to make one full revolution of the
-      // color wheel (range of 65536) along the length of the strip
-      // (strip.numPixels() steps):
-      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
-      // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
-      // optionally add saturation and value (brightness) (each 0 to 255).
-      // Here we're using just the single-argument hue variant. The result
-      // is passed through strip.gamma32() to provide 'truer' colors
-      // before assigning to each pixel:
-      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
-    }
-    strip.show(); // Update strip with new contents
-    delay(wait);  // Pause for a moment
+// Função para efeitos no NeoPixel
+uint32_t Wheel(byte WheelPos)
+{
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85)
+  {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
   }
+  if (WheelPos < 170)
+  {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
 
-void setup() {
-  pinMode(7, OUTPUT);
-  pinMode(7, HIGH);
-  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
-  strip.show();            // Turn OFF all pixels ASAP
-  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
-  myservo1.attach(9);  // attaches the servo on pin 9 to the servo object
-  myservo2.attach(8);  // attaches the servo on pin 9 to the servo object
- }
-// loop() function -- runs repeatedly as long as board is on ---------------
-
-void loop() {
-  //rainbow(50);
-if((millis() - lastUpdate) > updateInterval)  // time to update
+// Classe para servos
+class Sweeper
+{
+  Servo servo;              // the servo
+  int pos;              // posição do servo
+  int increment;        // incremento para mover a cada intervalo
+  int  updateInterval;      // intervalo entre updates
+  unsigned long lastUpdate; // última atualização
+ 
+public: 
+  Sweeper(int interval){
+    updateInterval = interval;
+    increment = 1;
+  }
+  
+  void Attach(int pin){
+    servo.attach(pin);
+  }
+  
+  void Detach(){
+    servo.detach();
+  }
+  
+  void Update()
+  {
+    // Variável de contagem j iniciado em 0
+    static uint8_t j = 0;
+    // Função millis para contagem de tempo
+    if((millis() - lastUpdate) > updateInterval)  // time to update
     {
       lastUpdate = millis();
       pos += increment;
-      myservo1.write(pos);
-      myservo2.write(pos);
-      if ((pos >= 20) || (pos <= 120)) // end of sweep
+      servo.write(pos);
+      strip.setBrightness(80);
+      // Posições iniciais e finais dos servos
+      if ((pos >= 70) || (pos <= 0)) 
       {
-        // reverse direction
+        // Muda direção de movimento
         increment = -increment;
       }
-    }
+      // Contador para acender NeoPixels
+      for (uint16_t Pixel = 0; Pixel < strip.numPixels(); Pixel++){       
+          strip.setPixelColor(Pixel, Wheel(((Pixel * 256 / strip.numPixels()) + j) & 255));
+        }
+        // Acende NeoPixels
+        strip.show();
+        // Incrementa variável j
+        j++;
+     }
   }
+};
+ 
+// Objetos sweeper1 e 2, referente à classe Sweeper 
+Sweeper sweeper1(25);
+Sweeper sweeper2(25);
+ 
+void setup() { 
+  // Objetos anexados aos pinos dos servos
+  sweeper1.Attach(8);
+  sweeper2.Attach(9);
+  // Início dos efeitos NeoPixel
+  strip.begin();
+  strip.show();
+} 
+ 
+void loop() { 
+  // Função para servos e NeoPixels
+  sweeper1.Update();
+  sweeper2.Update();
+}
